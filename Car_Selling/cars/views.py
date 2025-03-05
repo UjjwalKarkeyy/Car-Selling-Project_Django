bc404ebc221fe0a34183e1d4b0217b8adfe24e80
UjjwalKarkeyy <ujjwalkarki.official@gmail.com>
2025-03-05 20:03:45 +0545

    list-view-template-done

--- Car_Selling/cars/views.py
+++ Car_Selling/cars/views.py
@@ -6,6 +6,16 @@ class CarsListView(ListView):
     model = Cars
     context_object_name = 'cars'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Cars.objects.values_list('category', flat = True).distinct()
+        cars_by_category = {category: Cars.objects.filter(category = category) for category in categories}
+
+        context['cars_by_category'] = cars_by_category
+
+        return context
+
+
 class CarsDetailView(DetailView):
     pass
 
